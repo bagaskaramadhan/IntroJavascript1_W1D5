6d76277815fc5c66080f0350a0f56b6fc6e3549b
Bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-08-08 15:36:26 +0700

    task3

--- Task3.js
+++ Task3.js
@@ -1,25 +1,11 @@
-let spc = '';
-let num = 3;
-
-for (let i = num; i >= 1; i--) {
-    for (let j = 1; j <= i; j++) {
-
-        spc += j;
+printSegitiga = (num) =>{
+    for(let i = num; i>=1; i--){
+        let bL = '';
+        for(let j = 1; j<=i;j++){
+            bL += j + ' '
+        }
+        console.log(bL);
     }
-    console.log(spc)
-    spc = ''
 }
-// asd = (num) => {
-//     let spc = '';
 
-//     for (let i = num; i >= 1; i--) {
-//         for (let j = 1; j <= i; j++) {
-
-//             spc += j;
-//         }
-//         console.log(spc)
-//         spc = ''
-//     }
-
-// }
-// asd(4)
+printSegitiga(5);
